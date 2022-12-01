304a9539f90f3632c7c15bbb3f9a805a16bd0c5f
Sir Caedus <juanmartingonzalezmansilla@gmail.com>
2022-11-30 21:06:39 -0300

    Shop transformada de contenido estático a contenido dinámico. Cambio en nombres de imágenes

--- js/main.js
+++ js/main.js
@@ -1,72 +1,110 @@
-const items = /* [
-    {id: 1, clase: 'Ball',  nombre: 'Poke Ball', precio: 20, stock: 100, descripcion:'La ball más común. Sirve para capturar Pokémon salvajes.'},
-    {id: 2, clase: 'Ball',  nombre: 'Super Ball', precio: 60, stock: 100, descripcion:'Es buena. Tiene más índice de éxito en capturas que la Poke Ball.'},
-    {id: 3, clase: 'Ball', nombre: 'Ultra Ball', precio: 120, stock: 100, descripcion:'Es muy buena. Tiene más índice de éxito que la Super Ball.'},
-    {id: 4, clase: 'Ball', nombre: 'Lujo Ball', precio: 100, stock: 100, descripcion:'Es muy acogedora. Hace a los Pokémon más simpáticos.'},
-    {id: 5, clase: 'Ball', nombre: 'Honor Ball', precio: 20, stock: 100, descripcion:'Es muy particular. Se creó para una conmemoración.'},
-    {id: 6, clase: 'Ball', nombre: 'Nido Ball', precio: 100, stock: 100, descripcion:'Es excelente para capturar Pokémon de menor nivel.'},
-    {id: 7, clase: 'Ball', nombre: 'Malla Ball', precio: 100, stock: 100, descripcion:'Sirve especialmente para capturar Pokémon de tipo Agua y Bicho.'},
-    {id: 8, clase: 'Ball', nombre: 'Turno Ball', precio: 100, stock: 100, descripcion:'Aumenta la eficacia de captura cuanto más avanzado esté el combate.'},
-    {id: 9, clase: 'Ball', nombre: 'Buceo Ball', precio: 100, stock: 100, descripcion:'Aumenta la eficacia de captura con Pokémon del fondo del mar.'},
-    {id: 10, clase: 'Ball', nombre: 'Acopio Ball', precio: 100, stock: 100, descripcion:'Aumenta la eficacia de captura con Pokémon de la misma especie a los capturados previamente.'},
-    {id: 11, clase: 'Ball', nombre: 'Sana Ball', precio: 30, stock: 100, descripcion:'Es curativa. Restaura los Puntos de Salud del Pokémon capturado, y cura sus efectos de estado.'},
-    {id: 12, clase: 'Ball', nombre: 'Ocaso Ball', precio: 100, stock: 100, descripcion:'Hace que sea más fácil capturar Pokémon en la oscuridad.'},
-    {id: 13, clase: 'Ball', nombre: 'Veloz Ball', precio: 100, stock: 100, descripcion:'Tiene un índice de captura mayor si se utiliza al inicio de un encuentro con un Pokémon.'},
-    {id: 14, clase: 'Ball', nombre: 'Master Ball', precio: 10000, stock: 100, descripcion:'La Ball definitiva. Atrapa sin fallar nunca.'},
-    {id: 15, clase: 'Med', nombre: 'Poción', precio: 30, stock: 100, descripcion:'Restaura 20 Puntos de Salud a un Pokémon.'},
-    {id: 16, clase: 'Med', nombre: 'Super Poción', precio: 70, stock: 100, descripcion:'Restaura 50 Puntos de Salud a un Pokémon.'},
-    {id: 17, clase: 'Med', nombre: 'Hiper Poción', precio: 120, stock: 100, descripcion:'Restaura 200 Puntos de Salud a un Pokémon.'},
-    {id: 18, clase: 'Med', nombre: 'Poción Máxima', precio: 250, stock: 100, descripcion:'Restaura todos los Puntos de Salud de un Pokémon.'},
-    {id: 19, clase: 'Med', nombre: 'Restaura Todo', precio: 300, stock: 100, descripcion:'Restaura todos los Puntos de Salud y alivia todos los efectos de estado de un Pokémon.'},
-    {id: 20, clase: 'Med', nombre: 'Antídoto', precio: 10, stock: 100, descripcion:'Cura el estado de envenenamiento.'},
-    {id: 21, clase: 'Med', nombre: 'Antiparaliz', precio: 20, stock: 100, descripcion:'Cura el estado de parálisis.'},
-    {id: 22, clase: 'Med', nombre: 'Antiquemar', precio: 25, stock: 100, descripcion:'Cura el estado de quemado.'},
-    {id: 23, clase: 'Med', nombre: 'Antihielo', precio: 25, stock: 100, descripcion:'Cura el estado de congelamiento.'},
-    {id: 24, clase: 'Med', nombre: 'Despertar', precio: 25, stock: 100, descripcion:'Cura el estado de dormido.'},
-    {id: 25, clase: 'Med', nombre: 'Cura Total', precio: 60, stock: 100, descripcion:'Cura cualquier problema de estado.'},
-    {id: 26, clase: 'Med', nombre: 'Revivir', precio: 150, stock: 100, descripcion:'Revive a un Pokémon debilitado y restaura la mitad de sus Puntos de Salud.'},
-
-]; */ [];
+const items = [];
 
 let carrito = [];
+
+const pedirItems = async () => {
+    const resp = await
+    fetch('../js/data.json')
+    const data = await resp.json()
+
+    data.forEach ( (item) => {
+        items.push({id: item.id, nombre: item.nombre, precio: item.precio})
+
+        if (item.clase === 'Ball'){
+            domBall.innerHTML += `
+                    <div class="col">
+                        <div class="card h-100 items">
+                            <img src="assets/images/${item.clase}/${item.nombre}.webp" class="card-img-top imagesSizeBalls" alt="Imagen de ${item.nombre}">
+                            <div class="card-body">
+                                <h5 class="card-title">${item.nombre}</h5>
+                                <p class="card-text">${item.descripcion}</p>
+                                <p class="card-text">Precio: $${item.precio}</p>
+                                <p class="card-text">Stock: ${item.stock}</p>
+
+                                <p class="col-md-3 col-lg-3 col-xl-5 d-flex">
+                                    <button class="btn btn-light px-2" onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                                        -
+                                    </button>
+
+                                    <input class="inputAdd form-control form-control-sm" id="inputId${item.id}" data-id=${item.id} min="0" max="20" name="quantity" value="1" type="number" readonly/>
+                                    
+                                    <button class="btn btn-light px-2" onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                                        +
+                                    </button>
+                                </p>
+
+                                <button class="btn btn-primary btnAdd" data-id=${item.id}>Agregar al Carrito</button>                
+                            </div>
+                        </div>
+                    </div>`
+        } else {
+            domMed.innerHTML += `
+                    <div class="col">
+                        <div class="card h-100 items">
+                            <img src="assets/images/${item.clase}/${item.nombre}.webp" class="card-img-top imagesSizeMeds" alt="Imagen de ${item.nombre}">
+                            <div class="card-body">
+                                <h5 class="card-title">${item.nombre}</h5>
+                                <p class="card-text">${item.descripcion}</p>
+                                <p class="card-text">Precio: $${item.precio}</p>
+                                <p class="card-text">Stock: ${item.stock}</p>
+
+                                <p class="col-md-3 col-lg-3 col-xl-5 d-flex">
+                                    <button class="btn btn-light px-2" onclick="this.parentNode.querySelector('input[type=number]').stepDown()">
+                                        -
+                                    </button>
+
+                                    <input class="inputAdd form-control form-control-sm" id="inputId${item.id}" data-id=${item.id} min="0" max="20" name="quantity" value="1" type="number" readonly/>
+                                    
+                                    <button class="btn btn-light px-2" onclick="this.parentNode.querySelector('input[type=number]').stepUp()">
+                                        +
+                                    </button>
+                                </p>
+
+                                <button class="btn btn-primary btnAdd" data-id=${item.id}>Agregar al Carrito</button>                
+                            </div>
+                        </div>
+                    </div>`
+                
+        };
+
+    }); 
+
+    cargarLocalStorage()
+};
 
 const btnVaciar = document.getElementById('btnVaciar')
 const btnComprar = document.getElementById('btnComprar')
 
+
+
 const btnAdd = [...document.querySelectorAll('.btnAdd')]
 const inputAdd = [...document.querySelectorAll('.inputAdd')]
+
 //const inputMinus = [...document.querySelectorAll('.inputMinus')]
 //const inputPlus = [...document.querySelectorAll('.inputPlus')]
 
 const domCarrito = document.getElementById('carrito')
 const domTotal = document.getElementById('total')
 
-const input1 = document.getElementById('input1')
+const domBall = document.getElementById('Ball')
+const domMed = document.getElementById('Med')
 
-document.addEventListener('DOMContentLoaded',()=>{   
-    btnAdd.forEach((btn) => {
+
+document.addEventListener('DOMContentLoaded',()=>{  
+
+   /* btnAdd.forEach((btn) => {
         btn.onclick = (evt) =>{
             evt.preventDefault()
 
             let id = evt.target.getAttribute('data-id')
-            let counter = 0
-            let i = 1 
+            let counter = document.getElementById(`inputId${id}`)
 
-            inputAdd.forEach((input) => {
-               if (input.getAttribute('data-id') === id) {
-                counter = input.value
-               };
-            });
-
-            do {
-                carrito.push(evt.target.getAttribute('data-id'))
-                i++
-            } while (i <= counter);
+            carrito.push(counter.value)
 
             renderizarCarrito()
             actualizarLocalStorage()
         };
-    });
+    }); */
 
    /* inputMinus.forEach((btn) => {
         btn.onclick = (evt) => {
@@ -83,6 +121,9 @@ document.addEventListener('DOMContentLoaded',()=>{
     }); */
 
 
+    domBall.addEventListener('click', agregarCarrito)
+    domMed.addEventListener('click', agregarCarrito)
+
     btnVaciar.addEventListener('click',(evt)=>{
         evt.preventDefault()
         vaciarCarrito()
@@ -94,16 +135,21 @@ document.addEventListener('DOMContentLoaded',()=>{
     });
 });
 
-const pedirItems = async () => {
-    const resp = await
-    fetch('../js/data.json')
-    const data = await resp.json()
+function agregarCarrito(evt){
+    if (evt.target.classList.contains("btnAdd")){
+        evt.preventDefault()
+        let id = evt.target.getAttribute('data-id')
+        let counter = document.getElementById(`inputId${id}`).value  
+        let i = 1     
 
-    data.forEach ( (item) => {
-        items.push({id: item.id, nombre: item.nombre, precio: item.precio})
-    }); 
-    
-    cargarLocalStorage()
+        do {
+            carrito.push(id)
+            i++
+        } while (i <= counter);
+
+        renderizarCarrito()
+        actualizarLocalStorage() 
+    };  
 };
 
 function renderizarCarrito() {
@@ -124,7 +170,7 @@ function renderizarCarrito() {
         nodo.textContent = `${UnidadesItem} x ${miItem[0].nombre} - $${miItem[0].precio}`
         const boton = document.createElement('button')
         boton.classList.add('btn', 'btn-danger', 'mx-5')
-        boton.textContent = 'X'
+        boton.textContent = '-'
         boton.style.marginLeft = '1rem'
         boton.dataset.item = item
         boton.addEventListener('click', borrarItemCarrito)
